a596d4111a539e80f4f6266e716bac97b7c77730
Prakriti Singh <prrakritisingh@gmail.com>
2023-01-10 15:47:05 +0530

    fifth

--- script.js
+++ script.js
@@ -2,13 +2,33 @@ console.log("hey");
 
 document.getElementById("button").addEventListener('click', function(){
     console.log("wuv");
-    let main= document.getElementsByClassName("billsection")[0];
+    let first= document.getElementsByClassName("orders")[0];
+    // let main1= document.getElementsByClassName("billsection")[0];
+    const text1= document.createTextNode("Your Order:  ");
+    const heading1= document.createElement("span");
     let item= document.getElementById("item").value;
     const child1= document.createElement("span");
+    heading1.classList.add("text");
     child1.classList.add("order");
+    heading1.appendChild(text1);
+    first.appendChild(heading1);
     child1.innerText = item;
-    main.appendChild(child1);
-    console.log(child1);
+    first.appendChild(child1);
+    // console.log(heading);
+    // console.log(child1);
+    let second= document.getElementsByClassName("number")[0];
+    const heading2= document.createElement("span");
+    const text2= document.createTextNode("Quantity:  ");
+    let count= document.getElementById("quant").value;
+    const child2= document.createElement("span");
+    child2.classList.add("order");
+    heading2.classList.add("text");
+    heading2.appendChild(text2);
+    second.appendChild(heading2);
+    child2.innerHTML=count;
+    second.appendChild(child2);
+    console.log(child2);
+
 } )  
 
 
